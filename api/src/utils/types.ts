5a2faa9b428f4e97837fff5822e2ae1ba0effefd
47JMC <mjabir.5b@gmail.com>
2026-04-19 19:37:48 +0530

    add type for Room

--- api/src/utils/types.ts
+++ api/src/utils/types.ts
@@ -3,4 +3,16 @@ export type UserData = {
   username: string;
   avatar: string;
   global_name: string;
+  clicks: number;
+};
+
+export type RoomType = {
+  code: String;
+  host: UserData;
+  guest: UserData;
+  status: "waiting" | "countdown" | "running" | "done";
+  duration: Number;
+  startedAt: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
 };
